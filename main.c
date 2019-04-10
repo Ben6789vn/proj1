3c25aad46b7c5ade46d081354d23c514a37fc0e7
Ben Ha <c3278396@uon.edu.au>
2019-04-10 07:30:16 +0000

    Declare definition and write command to encrypt letters

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include<tdio.h>
+#include<string.h>
+
+
+    
+    void encryptMessage (char str[]);
+    
 int main() {
     
-    
-    asdf
-    asdf
-    asdf
-    asdf
-    asdf
-    asdf
-    asdf
+    void encryptMessage( char string[]){
+        int length = strlength(string);
+        char encrypt[length];
+        int i=0;
+        int count=0;
+        
+        for (i=0;i<length; i++)
+        {
+            if (string[i]=65)
+            {
+                encrypted[i]=90;
+                
+            }
+            else if (string[i]=90)
+            {
+                encrypted[i]=65;
+            
+            }
+            else
+            {
+                encrypted[i]= tring[i]-1;
+                
+            }
+            printf("")
+        }
+    }
+   
 return 0;
 }
