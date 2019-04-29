123aae2e69545f3fc5c777da8a08697d9178f84d
Ben Ha <c3278396@uon.edu.au>
2019-04-29 04:06:09 +0000

    edit code

--- main.c
+++ main.c
@@ -1,38 +1,34 @@
-#include<tdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 
-    
-    void encryptMessage (char str[]);
-    
-int main() {
-    
-    void encryptMessage( char string[]){
-        int length = strlength(string);
-        char encrypt[length];
-        int i=0;
-        int count=0;
-        
-        for (i=0;i<length; i++)
-        {
-            if (string[i]=65)
-            {
-                encrypted[i]=90;
-                
-            }
-            else if (string[i]=90)
-            {
-                encrypted[i]=65;
-            
-            }
-            else
-            {
-                encrypted[i]= tring[i]-1;
-                
-            }
-            printf("")
-        }
-    }
+void encryptMessage(char str[]);
+
+int main()
+{
    
-return 0;
+    encryptMessage("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    return 0;
 }
+
+// may declare key at the top and have more options
+void encryptMessage(char string[]){
+    int length = strlen(string);
+    char encrypted[length];
+    int i = 0;
+    int count = 0;
+    for(i=0; i<length; i++){
+        if(string[i] == 65){
+            encrypted[i] = 90;
+        }
+        else if(string[i] == 90){
+            encrypted[i] = 65;
+            count++;
+        }else {
+            encrypted[i] = string[i] - 1;    
+        }
+        
+    }
+    printf("The String is: %s and its count is %d",encrypted,count);
+}
+
